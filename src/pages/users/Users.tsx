76f2dbc9a6c675e7674d37854baa58e1eb98fad1
Amruta <amrutasadhu2519@gmail.com>
2023-07-23 15:31:19 +0530

    use user using API or add data manually.

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -86,29 +86,31 @@ const columns: GridColDef[] = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, status:"true"},
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, status:"false"},
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+// const rows = [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35, status:"true"},
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42, status:"false"},
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
 
 const Users = () => {
-  const [open,setOpen] = useState(false)
+  const [open,setOpen] = useState(false);
 
-  const {isLoading, data } = useQuery({
-    queryKey: ["allusers"],
-    queryFn: () =>
+  // TEST THE API
+
+  // const {isLoading, data } = useQuery({
+  //   queryKey: ["allusers"],
+  //   queryFn: () =>
       
-      fetch("http://localhost:8800/api/users").then(  // already data exists in database directly call that using API
-        (res) => res.json(),
-      ),
-  });
+  //     fetch("http://localhost:8800/api/users").then(  // already data exists in database directly call that using API
+  //       (res) => res.json(),
+  //     ),
+  // });
 
 
   return (
@@ -117,11 +119,15 @@ const Users = () => {
         <h1>Users</h1>
         <button onClick={()=>setOpen(true)}>Add New User</button>
       </div>
-       {isLoading ? 
+      <DataTable slug="users" columns={columns} rows={userRows}/>
+
+      {/* TEST THE API */}
+       {/* {isLoading ? 
        ("Loading..." ) 
        : 
        (<DataTable slug="users" columns={columns} rows={data}/>
-       )}
+       )} */}
+       
        {open && <Add setOpen={setOpen} slug="user" columns={columns}/>}
 
     </div>
